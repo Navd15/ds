566e8c66d42d7379752e157d43c59a36f236ad17
Navd15 <gill.222navdeep@gmail.com>
2018-08-15 23:59:12 +0530

    Red black trees in py

---
+++ python/Balanced trees/RB trees.py
@@ -0,0 +1,22 @@
+class Tree:
+  
+	__size=0 
+	__root=None
+
+
+    class __Node:
+        __Red=True
+        __Black=False      
+
+		def __init__(self,key,value,color):
+		   self.key=key
+           self.value=value
+           self.color=False
+
+    def __isRed(self,node):
+        if node==None:
+            return False
+        return True    
+    
+
+    
