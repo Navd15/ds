be79ac864772d786435b6471842f8fedb9e62421
Navd15 <gill.222navdeep@gmail.com>
2018-09-17 23:36:04 +0530

    Api for cyclic graph

--- java/Graphs/DFS.java
+++ java/Graphs/DFS.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
 import java.util.LinkedList;
@@ -11,6 +12,7 @@ public class DFS {
     private int[] edgeTo;
     /* Vertex we want to query about*/
     private int vt;
+
     /*Optional for client tests*/
     private List<Integer> search;
 
@@ -26,17 +28,17 @@ public class DFS {
     /*Path searching with depth-first search*/
 
     private void dfs(Graph g, int s) {
-        /*Always put quering vertex in array*/
+        /*Always put first quering vertex in array*/
         marked[s] = true;
 
         /*Optional*/
 
         search.add(s);
 
-        /*For each adjacent vertex to quering vertex i.e s 
-        * Check if it is not marked and call dfs again but this time with 
-        * adjacent vertices of s one by one
-        * */
+        /*For each adjacent vertex to quering vertex i.e s
+         * Check if it is not marked and call dfs again but this time with
+         * adjacent vertices of s one by one
+         * */
         for (int v : g.adj(s)) {
 
             if (!marked[v]) {
@@ -50,13 +52,13 @@ public class DFS {
     }
 
     /*Check if there is a path between s (quering vertex) and v*/
-    
+
     public boolean hasPath(int v) {
         return marked[v];
     }
 
-    /*If there exists a path then put the vertices in Stack   in-occured when traveling from s to v*/
-    
+    /*If there exists a path then put the vertices in Stack  when traveling from s to v*/
+
     public Iterable<Integer> path(int v) {
         Stack<Integer> s = new Stack<>();
 
@@ -73,6 +75,31 @@ public class DFS {
 
         return s;
     }
+
+    /*It should be implemented with dfs method*/
+    public boolean isAnyCycle(Graph g){
+
+        boolean isAny=false;
+
+        for(int v=0;v<g.vert.length;v++){
+
+            LinkedList<Integer> list=new LinkedList<>();
+        for(int i:g.adj(v)){
+            list.add(i);
+        }
+            for (int vertex:g.adj(v)){
+                for(int nextVert:g.adj(vertex)){
+                    if(list.contains(nextVert)){
+                        isAny=true;
+                    }
+
+                }
+
+             }
+        }
+        return isAny;
+    }
+
 
     public void pr() {
         for (int s : search
