9c3d450113fdf1350c4cdbc73407a6aca63fcf2c
Navd15 <gill.222navdeep@gmail.com>
2018-09-21 18:26:39 +0530

    test client for cycle and topological sort

--- java/DirectedGraphs/runner.java
+++ java/DirectedGraphs/runner.java
@@ -2,16 +2,32 @@ public class runner {
 
 
     public static void main(String[] args) {
-         DiGraph diGraph=new DiGraph(3);
-         diGraph.addEdge(0,1);
+         DiGraph diGraph=new DiGraph(4);
          diGraph.addEdge(0,2);
-         diGraph.addEdge(1,3);
+         diGraph.addEdge(2,1);
+         diGraph.addEdge(1,0);
+        diGraph.addEdge(0,3);
 
-         for(int i :diGraph.adj(0)){
-             System.out.println(0+"->"+i);
+        CyclicDIGraph cd=new CyclicDIGraph(diGraph);
+        TopologicalSort ts=new TopologicalSort(diGraph,cd);
+        
 
 
-         }
+//        if(cd.cyclicPath()!=null){
+//            for (int i:
+//                 cd.cyclicPath()) {
+//                System.out.println(i);
+//            }
+//
+//        }
+//
+//        for (int i:bfs.ret()) {
+//        }
+
+//        System.out.println(i);
+
+
+
     }
 }
 
