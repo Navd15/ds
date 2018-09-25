5d623cbf8e1f07be6f265500d727aca2512fb247
Navd15 <gill.222navdeep@gmail.com>
2018-09-25 23:52:49 +0530

    Edge abstract class for WeightedGraph api

---
+++ java/EdgeWeightedGraph/Edge.java
@@ -0,0 +1,32 @@
+public class Edge implements Comparable<Edge>{
+    private final int v,w;
+    private final double weight;
+
+    public Edge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+public int eitherVertex(){
+        return v;
+
+}
+
+public int alternateVertex(int vertex){
+        if(vertex==v)
+            return w;
+        return v;
+
+}
+
+
+    @Override
+    public int compareTo(Edge o) {
+        if(this.weight<o.weight)
+            return -1;
+        if(this.weight>o.weight)
+            return +1;
+        return 0;
+    }
+}
