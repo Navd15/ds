9e1d16285dc5fb9c41df373584074f0c26bbc307
Navd15 <gill.222navdeep@gmail.com>
2018-09-26 23:24:45 +0530

    edges() method

--- java/EdgeWeightedGraph/EdgeGraph.java
+++ java/EdgeWeightedGraph/EdgeGraph.java
@@ -1,11 +1,15 @@
 import edu.princeton.cs.algs4.Bag;
 
+import java.util.LinkedList;
+
 public class EdgeGraph {
 /*N.o of vertices*/
-    private final int v;
+     final int V;
      private final Bag<Edge>[] adj;
+     private LinkedList<Edge> edges;
     public EdgeGraph(int v) {
-    this.v=v;
+    this.V=v;
+    edges=new LinkedList<Edge>();
     adj=(Bag<Edge>[]) new Bag[v];
     for(int i=0;i<v;i++){
         adj[i]=new Bag<Edge>();
@@ -13,6 +17,7 @@ public class EdgeGraph {
     }
     }
     public void addEdge(Edge edge){
+        edges.add(edge);
      int v=edge.eitherVertex(),w=edge.alternateVertex(v);
 adj[v].add(edge);
 adj[w].add(edge);
@@ -21,6 +26,8 @@ adj[w].add(edge);
     public Iterable<Edge> adj(int v){
         return adj[v];
     }
-
+ public Iterable<Edge> edges(){
+        return edges;
+ }
 
 }
